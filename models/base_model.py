148821c55e5272c2f3857ee5ee2de01b6d5713e6
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-02-01 13:02:26 -0600

    task 4 update on args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,17 @@ class Base Model
 
 
 class BaseModel:
-    def __init__(self, id=0):
+    def __init__(self, *args, **kwargs):
         """Initialize BaseModel instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Return a string representation of the instance."""
