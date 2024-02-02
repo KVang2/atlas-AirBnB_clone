1b416d004d4fc93f89ccf58671b23b1348f3fddf
kvang2 <kevinvaj88@hotmail.com>
2024-02-01 21:28:12 -0600

    console.py file

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Program that contains entry point of command interpreter:
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    intro = "Welcome to the command interpreter. Type 'help' for a list of commands."
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Exit the program"""
+        return True
+    
+    def do_EOF(self, arg):
+        """Exit the program on EOF"""
+        return True
+    
+    def do_help(self, arg):
+        """Show informations"""
+        super().do_help(arg)
+
+    def emptylin(self):
+        """Empty line"""
+        pass
+
+if __name__ == "__main__":
+    hbnb_cmd = HBNBCommand()
+    hbnb_cmd.cmdloop()
