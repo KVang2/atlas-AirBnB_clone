9e652c07df9e9adb084ba4e6b191e7432dbdc362
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-02-02 11:04:10 -0600

    remove self from new methode

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
+            storage.new()
 
     def __str__(self):
         """Return a string representation of the instance."""
