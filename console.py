96a27040816329d4a611857ae2cd906bdf76a116
kvang2 <kevinvaj88@hotmail.com>
2024-02-04 15:18:36 -0600

    update message on do update

--- console.py
+++ console.py
@@ -99,7 +99,7 @@ class HBNBCommand(cmd.Cmd):
             key = "{}.{}".format(class_name, instance_id)
             all_instances = storage.all()
             if key not in all_instances:
-                print("** no instance found **")
+                print("** class doesn't exist **")
                 return
             if len(args) < 3:
                 print("** attribute name missing **")
