6f65d1b5c4972d7a7b219e856ce23b304275d0ec
kvang2 <kevinvaj88@hotmail.com>
2024-02-05 16:30:18 -0600

    update

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ class Base Model
 """
 import uuid
 from datetime import datetime
-from models
+from models import storage
 
 
 class BaseModel:
@@ -20,6 +20,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Return a string representation of the instance."""
@@ -28,7 +29,6 @@ class BaseModel:
     def save(self):
         """Update the public instance attribute updated_at with the current datetime."""
         self.updated_at = datetime.now()
-        storage.new(self)
         storage.save()
 
     def to_dict(self):
