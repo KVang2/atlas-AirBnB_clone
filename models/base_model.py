f5cd23b3a745901270ab5a5f828ff92963adb256
kvang2 <kevinvaj88@hotmail.com>
2024-02-05 11:14:50 -0600

    testing this file on basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ class Base Model
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialize BaseModel instance."""
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -18,7 +19,6 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
         """Return a string representation of the instance."""
@@ -27,6 +27,7 @@ class BaseModel:
     def save(self):
         """Update the public instance attribute updated_at with the current datetime."""
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
